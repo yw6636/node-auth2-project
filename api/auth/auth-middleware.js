689c018eddf0faa3450bf1a662889c684576fc2c
Yongwoo Lee <yw6636@gmail.com>
2023-07-13 17:39:23 -0400

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,5 +1,6 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 const { findBy } = require('../users/users-model')
+const jwt = require('jsonwebtoken')
 
 const restricted = (req, res, next) => {
   /*
@@ -17,6 +18,18 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
+  const token = req.headers.authorization
+  if (!token) {
+    next({ status: 401, message: "Token required"})
+  }
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      next({ status: 401, message: "Token invalid"})
+    } else {
+      req.decodedToken = decodedToken
+      next()
+    }
+  })
 }
 
 const only = role_name => (req, res, next) => {
@@ -53,6 +66,7 @@ const checkUsernameExists = async (req, res, next) => {
   } catch (err) {
     next(err)
   }
+}
 
 
 
